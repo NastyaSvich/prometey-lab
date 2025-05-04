879714bd8bebabfce082fa494712205134c32d81
ASvich <ASvich@qulix.com>
2025-05-04 18:25:22 +0300

    Реализации секции "Причины довериться нам"

--- src/components/swiper/SwiperInit.js
+++ src/components/swiper/SwiperInit.js
@@ -1,18 +1,26 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import {Navigation} from 'swiper/modules';
 
-Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation]);
 
 export function initSwiper() {
-  new Swiper('.swiper', {
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+    new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.arrow.arrow-right',
+            prevEl: '.arrow.arrow-left',
+        },
+        grabCursor: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        breakpoints: {
+            // when window width is >=
+            941: {
+                slidesPerView: 2,
+            },
+            1190: {
+                slidesPerView: 3,
+            }
+        },
+    });
 }
