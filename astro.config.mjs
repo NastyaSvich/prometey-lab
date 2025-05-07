4e7beef5cdca8a026c48c7a085b750e48e136059
ASvich <ASvich@qulix.com>
2025-05-07 14:35:40 +0300

    Доработка публикации

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import path from 'path';
 
 export default defineConfig({
+    outDir: process.env.BUILD_DIR || 'dist',
+    base: '/prometey-lab/',
     vite: {
         resolve: {
-          alias: {
-            'src': path.resolve('./src'),
-            '@styles': path.resolve('./src/styles'),
-            '@assets': path.resolve('./src/assets')
-          },
+            alias: {
+                src: path.resolve('./src'),
+                '@styles': path.resolve('./src/styles'),
+                '@assets': path.resolve('./src/assets'),
+            },
         },
-      },
+    },
 });
