185aa5d4cd45fa13e11ab4d96f6adfb0524db115
npetrovets <lmikon007@gmail.com>
2025-05-07 14:57:14 +0300

    feat: config netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,18 @@
 import { defineConfig } from 'astro/config';
 import path from 'path';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
-    vite: {
-        resolve: {
-          alias: {
-            'src': path.resolve('./src'),
-            '@styles': path.resolve('./src/styles'),
-            '@assets': path.resolve('./src/assets')
-          },
+  vite: {
+      resolve: {
+        alias: {
+          'src': path.resolve('./src'),
+          '@styles': path.resolve('./src/styles'),
+          '@assets': path.resolve('./src/assets')
         },
       },
+    },
+
+  adapter: netlify(),
 });
