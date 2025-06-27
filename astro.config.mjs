1f3b2a5215e3faaec65bbfb7f601bbe234a12fd8
Nikita Petrovets <lmikon007@gmail.com>
2025-06-28 00:40:44 +0300

    добавлен sitemap и .env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import {defineConfig} from 'astro/config';
 import path from 'path';
 import relativeLinks from 'astro-relative-links';
+import { loadEnv } from "vite";
 
+import sitemap from '@astrojs/sitemap';
+
+const { PUBLIC_APP_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 export default defineConfig({
+    site: PUBLIC_APP_URL,
     outDir: 'build/dist',
-    integrations: [relativeLinks()],
+    integrations: [relativeLinks(), sitemap()],
     vite: {
         resolve: {
             alias: {
